8057d8924c0dc58d3c789ad16ea0b0a2fb79763d
Andrew <andrew.1995@outlook.com>
2020-10-31 17:02:17 -0300

    feat(*): base configuration project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home-authentication', pathMatch: 'full' },
+  {
+    path: 'home-authentication',
+    loadChildren: () =>
+      import('./pages/home-authentication/home-authentication.module').then(
+        (m) => m.HomeAuthenticationModule
+      ),
+  },
+  { path: '**', redirectTo: 'home-authentication'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
