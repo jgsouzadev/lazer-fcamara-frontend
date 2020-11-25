db9895e9204b2564cd9e7d5b250cbc8f026ee8c2
Jaime Mathias de Lara Bueno <jaimemathias@hotmail.com>
2020-11-25 09:11:52 -0300

    feat(queue-entry): new graphism's component

--- src/app/pages/queue-entry/queue-entry.module.ts
+++ src/app/pages/queue-entry/queue-entry.module.ts
@@ -16,10 +16,11 @@ import { AuthGuard } from 'src/app/auth.guard';
 import { GraphismsComponent } from './shared/components/graphisms/graphisms.component';
 import { UserOnGameComponent } from './components/user-on-game/user-on-game.component';
 import { ConfirmQuitComponent } from './shared/components/confirm-quit/confirm-quit.component';
+import { GraphismsOrangeDisplayComponent } from './shared/components/graphisms-orange-display/graphisms-orange-display.component';
 
 
 @NgModule({
-  declarations: [QueueEntryComponent, QueueInComponent, UserOnQueueComponent, PlatformListComponent, GraphismsComponent, UserOnGameComponent, ConfirmQuitComponent],
+  declarations: [QueueEntryComponent, QueueInComponent, UserOnQueueComponent, PlatformListComponent, GraphismsComponent, UserOnGameComponent, ConfirmQuitComponent, GraphismsOrangeDisplayComponent],
   imports: [
     CommonModule,
     QueueEntryRoutingModule,
