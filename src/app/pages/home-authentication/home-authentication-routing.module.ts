782cb969e81fff66dcc5250dbf66db556cf514fa
Jaime Mathias de Lara Bueno <jaimemathias@hotmail.com>
2020-11-29 04:53:20 -0300

    wip(password-recovery): create and link

--- src/app/pages/home-authentication/home-authentication-routing.module.ts
+++ src/app/pages/home-authentication/home-authentication-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeAuthenticationComponent } from './home-authentication.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component'
+import { PasswordRecoveryComponent } from './components/password-recovery/password-recovery.component'
 
 const routes: Routes = [
   { path: '', component: HomeAuthenticationComponent },
-  { path: 'sign-up', component: SignUpComponent }
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'recover-password', component: PasswordRecoveryComponent },
 ];
 
 @NgModule({
