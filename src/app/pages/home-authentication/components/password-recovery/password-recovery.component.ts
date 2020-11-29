94e82315d275dfd8e9bcda81a92ac48c680537b9
Jaime Mathias de Lara Bueno <jaimemathias@hotmail.com>
2020-11-29 05:56:53 -0300

    feat(*): create component n' style responsively

--- src/app/pages/home-authentication/components/password-recovery/password-recovery.component.ts
+++ src/app/pages/home-authentication/components/password-recovery/password-recovery.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-password-recovery',
@@ -7,9 +13,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordRecoveryComponent implements OnInit {
 
-  constructor() { }
+  recoverForm: FormGroup;
+  emailSent: boolean = false;
+  timer: number = 5
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private _snackBar: MatSnackBar,
+    private authService: AuthService,
+    private router: Router,
+    private httpClient: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.formBuilderUser();
   }
 
-}
+  formBuilderUser() {
+    this.recoverForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+    })
+  }
+
+  recoverPassword() {
+    this.httpClient.post(
+      "https://www.fakeapi.online/api/apis/jaimemathias/api/user/password-recovery",
+      //`${environment.apiUrl}/`, 
+      { email: this.recoverForm.value.email },
+    ).subscribe(() => 
+    {
+      this.emailSent = true
+      this.recoverForm.controls['email'].setErrors(null)
+
+      setTimeout(() => {
+        this.router.navigateByUrl('home-authentication')
+        this.timer--
+      }, 5000)
+    }, (error) => {
+      this.openSnackBar('Email inexistente', 'Fechar')
+      this.recoverForm.controls['email'].setErrors({'incorrect': true})
+      console.error(error)
+    })
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 5000,
+    });
+  }
+}   
